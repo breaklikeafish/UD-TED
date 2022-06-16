311864dd649623918b5e46c5aea0f552932f8173
Klara <klara.lennermann@student.uni-tuebingen.de>
2022-06-16 12:33:29 +0200

    Add doc option

--- ud_ted/cli.py
+++ ud_ted/cli.py
@@ -1,6 +1,7 @@
 import argparse
+import time
 
-from ud_ted.distance import ud_ted
+from ud_ted.distance import ud_ted, avg_ud_ted
 
 
 def main():
@@ -20,12 +21,18 @@ def main():
                         type=str,
                         help="the path to the file containing the second sentence",
                         metavar="file2")
-    parser.add_argument("--ids",
-                        nargs=2,
-                        type=str,
-                        required=False,
-                        help="the ids of the sentences to compare",
-                        metavar=("id1", "id2"))
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--ids",
+                       nargs=2,
+                       type=str,
+                       required=False,
+                       help="the ids of the sentences to compare",
+                       metavar=("id1", "id2"))
+    group.add_argument("--doc",
+                       action="store_true",
+                       help="compute the tree edit distance for every pair of parallel sentences in a treebank")
+
     parser.add_argument("--deprel",
                         action="store_true",
                         help="compare dependency relation")
@@ -37,13 +44,24 @@ def main():
     args = parser.parse_args()
 
     # Execute program
-    dist = ud_ted(file1=args.file1,
-                  file2=args.file2,
-                  id1=args.ids[0] if args.ids else None,
-                  id2=args.ids[1] if args.ids else None,
-                  deprel=args.deprel,
-                  upos=args.upos)
+    if args.doc:
+        start = time.time()
+        dist = avg_ud_ted(file1=args.file1,
+                          file2=args.file2,
+                          deprel=args.deprel,
+                          upos=args.upos)
+        end = time.time()
+    else:
+        start = time.time()
+        dist = ud_ted(file1=args.file1,
+                      file2=args.file2,
+                      id1=args.ids[0] if args.ids else None,
+                      id2=args.ids[1] if args.ids else None,
+                      deprel=args.deprel,
+                      upos=args.upos)
+        end = time.time()
     print(f"Tree edit distance: {dist}")
+    print(f"Time: {end - start}")
 
 
 if __name__ == "__main__":
--- ud_ted/distance.py
+++ ud_ted/distance.py
@@ -1,3 +1,5 @@
+import time
+
 import pyconll
 
 from typing import List, Optional, Tuple
@@ -26,6 +28,9 @@ def ud_ted(file1: str, file2: str,
     x_nodes, x_adj = load_sentence(file1, id1)
     y_nodes, y_adj = load_sentence(file2, id2)
 
+    if len(x_nodes) > 27 or len(y_nodes) > 27:
+        print(f"WARNING: Tree has {max(len(x_nodes), len(y_nodes))} nodes.")
+
     # Choose cost function
     cost_func = _choose_cost_func(deprel, upos)
 
@@ -35,24 +40,82 @@ def ud_ted(file1: str, file2: str,
     return distance
 
 
-def load_sentence(path: str, sent_id: Optional[str] = None) -> Tuple[List[Label], List[List[int]]]:
+def avg_ud_ted(file1: str, file2: str,
+               deprel: Optional[bool] = False,
+               upos: Optional[bool] = False
+               ) -> float:
+    """
+    Computes the tree edit distance between every pair of sentences in two CoNLL-U files.
+
+    :param file1: The path to the CoNLL-U file containing the first treebank
+    :param file2: The path to the CoNLL-U file containing the second treebank
+    :param deprel: Optional. Whether to compare the dependency relationship label
+    :param upos: Optional. Whether to compare the universal dependency tag
+    :return: The tree edit distance
+    """
+    i = 0
+    distances = []
+
+    # Choose cost function
+    cost_func = _choose_cost_func(deprel, upos)
+
+    print(f"ID\tDistance\tTime\tNum of Nodes")
+
+    while True:
+
+        # Load input
+        sent1 = load_sentence(file1, sent_num=i, return_id=True)
+        sent2 = load_sentence(file2, sent_num=i)
+
+        if sent1 is None or sent2 is None:
+            break
+
+        x_nodes, x_adj, sent_id = sent1
+        y_nodes, y_adj = sent2
+
+        # Compute distance
+        start = time.time()
+        x = uted_astar(x_nodes, x_adj, y_nodes, y_adj, delta=cost_func)
+        distance, alignment, n = x if x else (None, None, None)
+        end = time.time()
+
+        print(f"{sent_id}\t{distance}\t{end - start}sec\t{max(len(x_nodes), len(y_nodes))}")
+
+        if distance:
+            distances.append(distance)
+
+        i += 1
+
+    if len(distances) > 0:
+        return sum(distances)/len(distances)
+
+
+def load_sentence(path: str, sent_id: Optional[str] = None, sent_num: int = 0, return_id: bool = False) \
+        -> Tuple[List[Label], List[List[int]]] | Tuple[List[Label], List[List[int]], str]:
     """
     Loads the CoNLL-U sentence into an adjacency representation of the tree
 
     :param path: The path to the CoNLL-U file containing the sentence
     :param sent_id: Optional. The ID of the second sentence
-    :return: A tuple of a label list and an adjacency list
+    :param sent_num: The position of the sentence in the file (only if sent_id is None)
+    :param return_id: Whether to return the sentence ID if given
+    :return: A tuple of a label list and an adjacency list (and the sentence ID if return_id is True)
     """
-    for sentence in pyconll.load_from_file(path):
+    for i, sentence in enumerate(pyconll.load_from_file(path)):
         pyconll_tree = sentence.to_tree()
-        if not sent_id or sent_id == sentence.id:
+        if sent_id == sentence.id or (sent_id is None and sent_num == i):
             nodes = [Label(form=pyconll_tree.data.form, deprel=pyconll_tree.data.deprel, upos=pyconll_tree.data.upos)]
             child_index = 1
             adj = [[]]
             for token in pyconll_tree:
                 adj[0].append(child_index)
                 child_index = _add_child(token, nodes, adj, child_index)
-            return nodes, adj
+            while len(nodes) > len(adj):
+                adj.append([])
+            if return_id:
+                return nodes, adj, sentence.id
+            else:
+                return nodes, adj
 
 
 def _add_child(pyconll_tree: pyconll.tree.Tree, tree: List[Label], adj: List[List[int]], index: int) -> int:
